3ad727a2131369b682c30c70bc0032373f669dc8
Jack Nicol <jack.nicol@FVFGQ462Q05P-Pleo-MacBook-Pro.local>
2023-06-10 00:35:09 +0100

    add f# char parser

---
+++ parsers/fparser/Program.fs
@@ -0,0 +1,17 @@
+﻿open System
+
+type Parser<'T> = P of (string -> int -> Option<'T * int>)
+
+let pchar: Parser<char> = 
+    P <| fun s i->
+        if (i < s.Length) then
+            Some(s.[i], i+1)
+        else    
+            None
+
+let prun (P p) s = p s 0
+
+[<EntryPoint>]
+let main argv =
+    prun pchar "Hello jicol95, you are parsing a char" |> printfn "%A"
+    0 // exit code
