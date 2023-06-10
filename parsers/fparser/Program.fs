bd60af30899ed055ec28ed8cc36efdd63123db1e
Jack Nicol <jack.nicol@FVFGQ462Q05P-Pleo-MacBook-Pro.local>
2023-06-10 09:58:42 +0100

    added keyvalue parser

--- parsers/fparser/Program.fs
+++ parsers/fparser/Program.fs
@@ -3,55 +3,131 @@
 type Parser<'T> = P of (string -> int -> Option<'T * int>)
 
 let pchar: Parser<char> = 
-    P <| fun s i->
+    P <| fun (s: string) (i: int)->
         if (i < s.Length) then
             Some(s.[i], i+1)
         else    
             None
 
-let prun (P p) s = p s 0
+let prun (P p) (s: string) = p s 0
 
-let preturn v : Parser<'T> = 
-    P <| fun s i -> Some(v, i)
+let preturn (v: 'T) : Parser<'T> = 
+    P <| fun (s: string) (i: int) -> Some(v, i)
 
 let pfail () : Parser<'T> =
-    P <| fun s i -> None
+    P <| fun (s: string) (i: int) -> None
 
 let pbind (uf : 'T -> Parser<'U>) (P t) : Parser<'U> =
-    P <| fun s i -> 
+    P <| fun (s: string) (i: int) -> 
         match t s i with
         | None -> None
-        | Some (tv, ti) ->
+        | Some (tv: 'T, ti: int) ->
             let (P u) = uf tv
             u s ti
 
 let pcombine u t = t |> pbind (fun _ -> u)
 
+let pmany (P t) : Parser<'T list> = 
+    P <| fun (s: string) (pos: int) ->
+        let rec loop (vs: 'T list) (currentPos: int) =
+            match t s currentPos with
+            | None -> Some (List.rev vs, currentPos)
+            | Some (tvalue: 'T, tpos: int) -> loop (tvalue::vs) tpos
+        loop [] pos
+
+
 type ParserBuilder () =
     class
         // Enables let!
-        member x.Bind (t, uf) = pbind uf t
+        member x.Bind (t: Parser<'a>, uf) = pbind uf t
         // Enables do!
-        member x.Combine (t, u) = pcombine u t 
+        member x.Combine (t: Parser<'a>, u: Parser<'a>) = pcombine u t 
         // Enables return
-        member x.Return v = preturn v
+        member x.Return (v: 'a) = preturn v
         // Enables return!
-        member x.ReturnFrom p = p : Parser<'T> 
+        member x.ReturnFrom (p: Parser<'T>) = p : Parser<'T> 
         // Allows if x then expr with no else
         member x.Zero = preturn ()
     end
 
-let parser = ParserBuilder ()
+
+let parser: ParserBuilder = ParserBuilder ()
+
+let psatisfy (satisfy: char -> bool): Parser<char> = parser {
+    let! (char: char) = pchar
+    if satisfy char then 
+        return char
+    else 
+        return! pfail()
+}
+
+let pmany1 (t: Parser<'a>) = parser {
+    let! (head: 'a) = t
+    let! (tail: 'a list) = pmany t
+    return head::tail
+}
+
+let pmap map (t: Parser<'a>) = parser {
+    let! (v: 'a) = t
+    return map v
+}
+
+let makeint (vs: char list) =
+    let folder (aggr: int) (ch: char) = 
+        aggr * 10 + (int ch - int '0')
+
+    vs |> List.fold folder 0
+
+let pdigit: Parser<char> = psatisfy Char.IsDigit
+
+let pint: Parser<int> = pmany1 pdigit |> pmap makeint
+
+let pletter: Parser<char> = psatisfy Char.IsLetter
+
+let pwhitespace: Parser<char> = psatisfy Char.IsWhiteSpace
+
+let pskipchar ch = parser {
+    let! char = pchar
+    if ch = char then   
+        return ()
+    else
+        return! pfail ()
+}
+
+let padd = parser {
+    let! left = pint
+    do! pskipchar '+'
+    let! right = pint
+    return left + right
+}
+
+let pstring (t : Parser<char>) : Parser<string> =
+    pmany1 t |> pmap (fun charList -> charList |> List.toArray |> String)
+
+let pkeyvalue = parser {
+    let! left = pstring pletter
+    do! pskipchar '='
+    let! right = pint
+    return (left, right)
+}
+
+let pkeyvalues = parser {
+    let helper = parser {
+        let! ws = pmany1 pwhitespace
+        return! pkeyvalue 
+    }
+
+    let! head = pkeyvalue
+    let! tail = pmany helper
+    return (head, tail)
+}
 
 [<EntryPoint>]
 let main argv =
-    let p = parser {
-        let! first = pchar
-        let! second = pchar
-        let! third = pchar
-        return first, second, third
-    }
-
-    prun p "Hello jicol95, you are parsing a char" |> printfn "%A"
+    prun padd "4+2" |> printfn "%A"
+    prun pkeyvalues """dockerPort=8080
+        postgresPort=5045
+        universe=42
+    """ |> printfn "%A"
 
     0 // exit code
